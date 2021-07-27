8353db3872fbffd078c7a32a7b7a66c237df9cdb
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-07-27 21:31:58 +0500

    Menu disappears when we select a menu-item

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const hamburgerBtn = document.querySelector(".hamburger-menu");
 const crossBtn = document.querySelector(".cross");
 const mobileHeader = document.querySelector(".mobile-header");
 const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuItems = document.querySelector("ul.mobile-menu-items");
 
 hamburgerBtn.addEventListener("click", function (event) {
   mobileMenu.classList.toggle("d-block");
 });
 
 crossBtn.addEventListener("click", function (event) {
+  mobileMenu.classList.toggle("d-block");
+});
+
+mobileMenuItems.addEventListener("click", function (event) {
   mobileMenu.classList.toggle("d-block");
 });

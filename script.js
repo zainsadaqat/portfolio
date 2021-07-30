f7da4b102be7515e384b20a93046de5b63f3f7de
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-07-31 01:51:42 +0500

    Fix eslint issue

--- script.js
+++ script.js
@@ -42,19 +42,6 @@ form.addEventListener('submit', (e) => {
   }
 });
 
-const showErrors = (errors) => {
-  errors.forEach((error) => {
-    const li = document.createElement('li');
-    li.innerText = error;
-    li.classList.add('programming-language');
-    li.style.color = 'red';
-    li.style.marginBottom = '16px';
-    li.style.padding = '14px';
-    errorMessagesContainer.append(li);
-  });
-  errorMessagesContainer.classList.toggle('d-none');
-};
-
 const storeName = document.querySelector('input[type=text]');
 const storeEmail = document.querySelector('input[type=email]');
 const storeMessage = document.querySelector('.input-textarea');

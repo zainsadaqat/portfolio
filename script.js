1896b2b4a87267a013242ff467434645beb33174
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-07-30 22:51:12 +0500

    Implement client side form validation using javascript

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ crossBtn.addEventListener('click', () => {
 mobileMenuItems.addEventListener('click', () => {
   mobileMenu.classList.toggle('d-block');
 });
+
+const form = document.querySelector('.form');
+const validateEmail = document.querySelector('input[type=email]');
+const errorMessagesContainer = document.querySelector('.error-messages');
+form.addEventListener('submit', (e) => {
+  const errorMessages = [];
+  if (validateEmail.value !== validateEmail.value.toLowerCase()) {
+    errorMessages.push('Email should be in lowercase');
+  }
+  if (errorMessages.length > 0) {
+    e.preventDefault();
+    showErrors(errorMessages);
+  }
+});
+
+const showErrors = (errors) => {
+  errors.forEach((error) => {
+    const li = document.createElement('li');
+    li.innerText = error;
+    li.classList.add('programming-language');
+    li.style.color = 'red';
+    li.style.marginBottom = '16px';
+    li.style.padding = '14px';
+    errorMessagesContainer.append(li);
+  });
+  errorMessagesContainer.classList.toggle('d-none');
+};
